8dffcb538c9e278f0d77f45551607032d5bd9261
RakeshVattikonda1 <rakeshvattikonda00@gmail.com>
2025-05-29 12:35:58 -0500

    Click me Date and Time.

---
+++ Hello.js
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h1>My First JavaScript</h1>
+<button type="button" onclick="document.getElementById('rakesh').innerHTML = Date()" >
+    Clock me to display Date and Time.
+</button>
+<p id="rakesh"></p>
+</body>
+</html>
